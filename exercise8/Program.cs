98f5e143e59f8092804a5ef04a2eb9b431ab6ee4
lubov <loveavduhova@gmail.com>
2022-06-19 14:22:28 +0700

    done ex8

--- exercise8/Program.cs
+++ exercise8/Program.cs
@@ -5,12 +5,14 @@
 Console.WriteLine("Введите число: ");
 
 int number = int.Parse (Console.ReadLine());
- int count = 1;
-while (count <= number)
+
+
+for (int count = 1; count <= number; count++)
 {
     if (count % 2 == 0)
     {
-    Console.WriteLine (count);
+    Console.Write(count + ", ");
         count++;
-    }
-}
+    } 
+
+} 
